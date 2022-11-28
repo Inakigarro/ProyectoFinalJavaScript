25b996f7548d9b95c7288d17456e18cb45050a6e
inakigarro <inakigarro@gmail.com>
2022-11-28 19:17:23 -0300

    feat: input verification

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ let dni;
 let valorVerificacionDatosUsuario;
 
 let canchasDisponiblesArray = [1, 3, 5, 6, 7, 9, 10];
-let canchas = '';
+let canchas = "";
 let canchaSeleccionada;
 
 let turnosDisponiblesArray = [9, 10, 16, 20, 22];
-let turnos = '';
+let turnos = "";
 let turnoSeleccionado;
 
-let montoFinal
+let montoFinal;
 
 const valorPorTurnoDiario = 2500;
 const valorPorTurnoNocturno = 3000;
@@ -25,103 +25,125 @@ const valorPorTurnoNocturno = 3000;
  * - apellido: `string`
  * - dni: `stirng`
  */
-function InputDatosUsuario(){
-    nombre = prompt("Ingrese su nombre:");
-    console.log(`nombre: ${nombre}`)
+function InputDatosUsuario() {
+	nombre = prompt("Ingrese su nombre:");
+	console.log(`nombre: ${nombre}`);
 
-    apellido = prompt("Ingrese su apelido:");
-    console.log(`apellido: ${apellido}`)
-    
-    dni = prompt("Ingrese su DNI:");
-    console.log(`dni: ${dni}`)
+	apellido = prompt("Ingrese su apelido:");
+	console.log(`apellido: ${apellido}`);
+
+	dni = prompt("Ingrese su DNI:");
+	console.log(`dni: ${dni}`);
 }
 
 /**
  * Solicita al usuario la verificacion de los datos ingresados
  * En el caso de ser correctos, devuelve `true`. Caso contrario, `false`.
- * 
+ *
  * @return {boolean}
  */
-function verificacionDatosUsuario(){
-    let inputVerificacion;
+function verificacionDatosUsuario() {
+	let inputVerificacion;
 
-    inputVerificacion = prompt(`
+	inputVerificacion = prompt(`
         Compruebe los datos ingresados:
             - nombre: ${nombre}
             - apellido: ${apellido}
             - dni: ${dni}
         Si los datos ingresados son correctos, ingrese 'Si'`).toLowerCase();
-    console.log(`Los datos son correctos? ${inputVerificacion === 'si'}`);
+	console.log(`Los datos son correctos? ${inputVerificacion === "si"}`);
 
-    if (inputVerificacion !== 'si'){
-        return false;
-    }
+	if (inputVerificacion !== "si") {
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 /**
  * Recorre la lista de canchas disponibles y arma una cadena
  * para mostrar por pantalla.
- * 
+ *
  * @returns una cadena que contiene todas las canchas disponibles.
  */
-function canchasDisponibles(){
-    let disponibles = '';
+function canchasDisponibles() {
+	let disponibles = "";
 
-    canchasDisponiblesArray.forEach(cancha => {
-        disponibles = disponibles + `- cancha ${cancha} \n`
-    });
-    console.log(`Las canchas disponibles son:
+	canchasDisponiblesArray.forEach((cancha) => {
+		disponibles = disponibles + `- cancha ${cancha} \n`;
+	});
+	console.log(`Las canchas disponibles son:
 ${disponibles}`);
 
-    return disponibles;
+	return disponibles;
 }
 
 /**
  * Recorre la lista de turnos disponibles y arma una cadena
  * para mostrar por pantalla.
- * 
+ *
  * @returns una cadena que contiene todas los turnos disponibles.
  */
-function turnosDisponibles(){
-    let disponibles = '';
+function turnosDisponibles() {
+	let disponibles = "";
 
-    turnosDisponiblesArray.forEach(turno => {
-        disponibles = disponibles + `- turno ${turno} \n`
-    });
-    console.log(`Los turnos disponibles son:
+	turnosDisponiblesArray.forEach((turno) => {
+		disponibles = disponibles + `- turno ${turno} \n`;
+	});
+	console.log(`Los turnos disponibles son:
 ${disponibles}`);
 
-    return disponibles;
+	return disponibles;
 }
 
 /**
  * Solicita la seleccion de una cancha disponible para jugar.
- * 
- * @param {string} canchasDisponibles Lista de canchas disponibles para jugar. 
+ *
+ * @param {string} canchasDisponibles Lista de canchas disponibles para jugar.
  */
-function seleccionDeCancha(canchasDisponibles){
-    canchaSeleccionada = prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
-${canchasDisponibles}`);
+function seleccionDeCancha(canchasDisponibles) {
+    // Solicito la seleccion de una cancha para jugar.
+	canchaSeleccionada =
+		parseInt(prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
+${canchasDisponibles}`));
+
+    // Si la cancha seleccionada no se encuentra entre las disponibles
+    // Vuelvo a solicitar.
+	while (!canchasDisponiblesArray.includes(canchaSeleccionada)) {
+		canchaSeleccionada =
+		parseInt(prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
+${canchasDisponibles}`));
+	}
 }
 
 /**
  * Solicita la seleccion de un turno disponible para jugar.
- * 
- * @param {string} turnosDisponibles 
+ *
+ * @param {string} turnosDisponibles
  */
-function seleccionDeturno(turnosDisponibles){
-    turnoSeleccionado = parseInt(prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
-${turnosDisponibles}`));
+function seleccionDeturno(turnosDisponibles) {
+    // Solicito la seleccion de un turno disponible para jugar.
+	turnoSeleccionado = parseInt(
+		prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
+${turnosDisponibles}`)
+	);
+    
+    // Si el turno seleccionado no se encuentra entre los disponibles
+    // Vuelvo a solicitar.
+    while (!turnosDisponiblesArray.includes(turnoSeleccionado)) {
+        turnoSeleccionado = parseInt(
+        prompt(`Seleccione una de las siguientes canchas disponibles para jugar:
+${turnosDisponibles}`)
+        );
+    }
 }
 
 // Ejecucion del codigo.
 
 // Solicito el ingreso de datos basicos del usuario.
 do {
-    InputDatosUsuario();
-    valorVerificacionDatosUsuario = verificacionDatosUsuario();
+	InputDatosUsuario();
+	valorVerificacionDatosUsuario = verificacionDatosUsuario();
 } while (!valorVerificacionDatosUsuario);
 
 alert(`Bienvenido ${nombre}!`);
@@ -138,11 +160,14 @@ seleccionDeCancha(canchas);
 turnos = turnosDisponibles();
 seleccionDeturno(turnos);
 
-if(turnoSeleccionado <= 19){
-    montoFinal = valorPorTurnoDiario;
+if (turnoSeleccionado <= 19) {
+	montoFinal = valorPorTurnoDiario;
 } else {
-    montoFinal = valorPorTurnoNocturno;
+	montoFinal = valorPorTurnoNocturno;
 }
 
 alert(`El monto a pagar para el turno ${turnoSeleccionado} en la cancha ${canchaSeleccionada} es:
-- ${montoFinal}`)
+- ${montoFinal}`);
+console.log(`turno seleccionado: ${turnoSeleccionado}`);
+console.log(`cancha seleccionada: ${canchaSeleccionada}`);
+console.log(`monto final: ${montoFinal}`);
